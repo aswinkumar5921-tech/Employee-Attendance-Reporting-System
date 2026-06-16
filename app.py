132891e43ef4462cd0e58902cbc34276db052d9b
Your Name <your@email.com>
2026-06-16 17:53:47 +0530

    test stream

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+import pandas as pd
+import streamlit as st
+
+db=pd.read_csv("attendance.csv")
+
+st.set_page_config(page_title="Employee Attendance System", layout="wide")
+st.title("🏢 Employee Attendance & Reporting System")
+
 print("Hello World")
